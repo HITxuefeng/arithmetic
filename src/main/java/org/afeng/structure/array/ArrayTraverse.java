1d7da72ae5bcb0ecc03d5e47bdcdda6be95af246
xuefeng <1564665679@qq.com>
2021-07-12 15:48:09 +0800

    fit(dev): array

--- src/main/java/org/afeng/structure/array/ArrayTraverse.java
+++ src/main/java/org/afeng/structure/array/ArrayTraverse.java
@@ -1,11 +1,22 @@
 package org.afeng.structure.array;
 
+import java.util.Arrays;
+
 /**
  * @Author xuefeng
  * @Date 2021/7/9 5:20 下午
  */
 public class ArrayTraverse {
     public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int[] arrOut = traverse(arr);
+        System.out.println(Arrays.toString(arrOut));
+    }
 
+    public static int[] traverse(int[] arr) {
+        for(int i = 0; i < arr.length; i ++) {
+            arr[i] += 1;
+        }
+        return arr;
     }
 }
